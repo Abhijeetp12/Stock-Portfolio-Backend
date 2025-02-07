cb8f1996871003a37509cbbfb116d41e82d9ef8b
Abhijeetp12 <130688454+Abhijeetp12@users.noreply.github.com>
2025-02-07 10:00:03 +0530

    Update server.js

--- server.js
+++ server.js
@@ -9,31 +9,39 @@ import session from "express-session";
 import authRoutes from "./Routes/authRoutes.js";
 import stockRoutes from "./Routes/stockRoutes.js";
 import pool from "./Config/db.js";
+import pgSession from "connect-pg-simple";
 import cors from 'cors';
 import "./Config/Passport.js";
 
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:3001',  // Allow requests from the frontend
+  origin: "http://localhost:3001",  // Allow requests from the frontend
   methods: 'GET,POST,PUT,DELETE',   // Specify allowed methods (optional)
   allowedHeaders: 'Content-Type,Authorization', // Specify allowed headers (optional)
   credentials: true,  // Allow credentials (cookies, etc.)
 }));
 
-const port=process.env.PORT;
+const port=3000;
+
+const pgStore = pgSession(session);
 
 console.log(process.env.SESSION_SECRET);
 app.use(
   session({
+    store: new pgStore({
+      pool: pool, // Use your existing PostgreSQL pool
+      schemaName: "Stock_portfolio", 
+      tableName: "sessions", // Custom table name (default: 'session')
+    }),
     secret: process.env.SESSION_SECRET,
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: false,
     cookie: {
-      httpOnly: true, 
-      secure: false, 
-      sameSite: "lax",
       maxAge: 1000 * 60 * 60 ,
+      secure: false, 
+      httpOnly:true,
+      sameSite:"lax"
     },
   })
 );
@@ -48,19 +56,22 @@ app.use('/', mainRoutes);
 app.use('/auth', authRoutes);
 app.use('/portfolio',stockRoutes);
 
-console.log(process.env.DB_PASSWORD);
-
+console.log(process.env.PG_PASSWORD);
 async function testDatabaseConnection() {
   try {
-    const [rows] = await pool.execute('SELECT 1');
+    const { rows } = await pool.query('SELECT 1');
     console.log('Database connected successfully');
   } catch (err) {
     console.error('Error connecting to the database:', err);
-    process.exit(1);  // Exit the process if database connection fails
+    process.exit(1); // Exit the process if database connection fails
   }
 }
 
 testDatabaseConnection();
+
+const result = await pool.query("SHOW search_path");
+console.log("Current search path:", result.rows);
+
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
