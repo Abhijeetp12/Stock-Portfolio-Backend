ae2d37a9009343c35f6d7f6745ef139213eac4b1
Abhijeetp12 <130688454+Abhijeetp12@users.noreply.github.com>
2025-02-13 12:11:59 +0530

    Update server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(
       maxAge: 1000 * 60 * 60 ,
       secure: process.env.NODE_ENV === "production", 
       httpOnly:true,
-      sameSite:"none"
+      sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'lax'
     },
   })
 );
